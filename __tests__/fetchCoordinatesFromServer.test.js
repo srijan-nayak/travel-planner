e0e98261bd429280905caf08ca8cab20014c0069
Srijan Nayak <srijannayak0@gmail.com>
2021-02-20 21:12:02 +0530

    test: Add specifications for fetchCoordinatesFromServer

---
+++ __tests__/fetchCoordinatesFromServer.test.js
@@ -0,0 +1,23 @@
+import { fetchCoordinatesFromServer } from "../src/client/js/serverDataFetchers";
+
+describe("fetch coordinates from server", () => {
+  test("fetch with valid input", async () => {
+    const coordinates = await fetchCoordinatesFromServer("bangalore");
+    const { latitude, longitude } = coordinates;
+    expect(typeof latitude).toBe("string");
+    expect(typeof longitude).toBe("string");
+  });
+
+  test("fetch with invalid input", async () => {
+    const coordinatesFetchError = new Error(
+      "Could not fetch coordinates with given destination name!"
+    );
+
+    expect(fetchCoordinatesFromServer("")).rejects.toEqual(
+      coordinatesFetchError
+    );
+    expect(fetchCoordinatesFromServer("Baggalore")).rejects.toEqual(
+      coordinatesFetchError
+    );
+  });
+});
