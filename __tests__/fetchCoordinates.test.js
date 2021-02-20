92b6acd2907c0530b53bf2ad0b57f17ff94b10ea
Srijan Nayak <srijannayak0@gmail.com>
2021-02-20 16:59:57 +0530

    test: Test fetchers with invalid inputs

--- __tests__/fetchCoordinates.test.js
+++ __tests__/fetchCoordinates.test.js
@@ -1,11 +1,19 @@
 const { fetchCoordinates } = require("../src/server/apiDataFetchers");
 
-test("fetch coordinates", async () => {
-  const { ok, data } = await fetchCoordinates("bangalore");
-  expect(ok).toBe(true);
-  expect(data).toBeDefined();
+describe("fetch coordinates", () => {
+  test("fetch with valid input", async () => {
+    const { ok, data } = await fetchCoordinates("bangalore");
+    expect(ok).toBe(true);
+    expect(data).toBeDefined();
 
-  const { latitude, longitude } = data;
-  expect(typeof latitude).toBe("string");
-  expect(typeof longitude).toBe("string");
+    const { latitude, longitude } = data;
+    expect(typeof latitude).toBe("string");
+    expect(typeof longitude).toBe("string");
+  });
+
+  test("fetch with invalid input", async () => {
+    const { ok, data } = await fetchCoordinates("343hjk");
+    expect(ok).toBe(false);
+    expect(data).toBeNull();
+  });
 });
