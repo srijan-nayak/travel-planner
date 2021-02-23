176768b5433cf00c7aedf7a12f4276b3d6311517
Srijan Nayak <srijannayak0@gmail.com>
2021-02-23 10:27:55 +0530

    feat: Show loading animation in button while waiting to add trip

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -2,13 +2,20 @@ import fetchTripData from "./fetchTripData";
 import createTripCard from "./createTripCard";
 
 const addTrip = async () => {
+  const addTripButton = document.querySelector("#add-trip");
+  addTripButton.classList.add("button--loading");
   try {
     const tripData = await fetchTripData();
     const tripCard = createTripCard(tripData);
     document.querySelector(".no-trips").classList.add("no-trips--hidden");
     document.querySelector(".trips-container").appendChild(tripCard);
+
+    document.querySelector("#location").value = "";
+    document.querySelector("#date").value = "";
   } catch (error) {
     alert(error);
+  } finally {
+    addTripButton.classList.remove("button--loading");
   }
 };
 
