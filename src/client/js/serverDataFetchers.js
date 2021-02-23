7dff8e078d6026142fed3bb61f04016fcba756ae
Srijan Nayak <srijannayak0@gmail.com>
2021-02-23 21:33:30 +0530

    fix: Throw locationPictureFetchError in catch block

--- src/client/js/serverDataFetchers.js
+++ src/client/js/serverDataFetchers.js
@@ -54,7 +54,7 @@ const fetchLocationPictureURLFromServer = async (locationName) => {
     }
     return data;
   } catch {
-    locationPictureURLFetchError;
+    throw locationPictureURLFetchError;
   }
 };
 
