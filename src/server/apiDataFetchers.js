3e5e85b04039c8c39edf20667fa84c729f90070b
Srijan Nayak <srijannayak0@gmail.com>
2021-02-24 09:33:54 +0530

    doc: Add some comments in apiDataFetchers

--- src/server/apiDataFetchers.js
+++ src/server/apiDataFetchers.js
@@ -6,24 +6,28 @@ const fetchLocationData = async (query) => {
   const geonamesUsername = process.env.GEONAMES_USERNAME;
   const queryParams = {
     q: query,
+    // fetch only one 'row' of data
     maxRows: 1,
     username: geonamesUsername,
   };
 
   try {
     const response = await axios.get(apiRoot, { params: queryParams });
+
     const {
       lat: latitude,
       lng: longitude,
       name: locationName,
       countryName,
     } = response.data.geonames[0];
+    // check if all required data was fetched
     const ok =
       latitude !== undefined &&
       longitude !== undefined &&
       locationName !== undefined &&
       countryName !== undefined;
     const coordinates = { latitude, longitude };
+    // return null if data was not fetched properly
     const data = ok ? { locationName, countryName, coordinates } : null;
     return { ok, data };
   } catch {
@@ -44,16 +48,20 @@ const fetchWeatherForecast = async (days, coordinates) => {
 
   try {
     const response = await axios.get(apiRoot, { params: queryParams });
+
     const { data: forecastArray } = response.data;
+    // extract the last forecast to get the forecast only for the given day
     const {
       temp: temperature,
       pop: precipitation,
       weather: { description },
     } = forecastArray[days - 1];
+    // check if all required data was fetched
     const ok =
       temperature !== undefined &&
       precipitation !== undefined &&
       description !== undefined;
+    // return null if data was not fetched properly
     const data = ok ? { temperature, precipitation, description } : null;
     return { ok, data };
   } catch {
@@ -66,6 +74,7 @@ const fetchLocationPictureURL = async (locationName) => {
   const pixabayApiKey = process.env.PIXABAY_API_KEY;
   const queryParams = {
     q: locationName,
+    // fetch only horizontal 'travel' tagged photos
     category: "travel",
     image_type: "photo",
     orientation: "horizontal",
@@ -75,9 +84,13 @@ const fetchLocationPictureURL = async (locationName) => {
 
   try {
     const response = await axios.get(apiRoot, { params: queryParams });
+
     const { hits: searchResults } = response.data;
+    // extract picture url from the first search result only
     const { webformatURL: locationPictureURL } = searchResults[0];
+    // check if all required data was fetched
     const ok = locationPictureURL !== undefined;
+    // return null if data was not fetched properly
     const data = ok ? { locationPictureURL } : null;
     return { ok, data };
   } catch {
